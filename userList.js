4fb00cdc4488c905447bbd2196ac1306daa4d5ee
ChaimaAbde <chaimaabdedaime@gmail.com>
2022-12-02 12:56:28 +0100

    express

---
+++ userList.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    name: "ali",
+    age: "25",
+  },
+  {
+    name: "sami",
+    age: "27",
+  },
+  {
+    name: "aya",
+    age: "45",
+  },
+  {
+    name: "nour",
+    age: "35",
+  },
+];
+module.exports = users;
